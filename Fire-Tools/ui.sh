a56039b2690a99094cbc9a1d04b587d6d4c463ee
Hayden Plumley <mrhaydendp@gmail.com>
2021-12-15 20:46:06 +0000

    Update ui.sh

--- Fire-Tools/ui.sh
+++ Fire-Tools/ui.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Show the Message of the Day
+curl -sSL https://github.com/mrhaydendp/Fire-Tools/raw/main/Motd
+
 # Identify Device & List Model
 device=$(adb shell getprop ro.product.model)
     [ "$device" = "KFMUWI" ] && device="Fire 7 (9th Gen)" && sup="1"
