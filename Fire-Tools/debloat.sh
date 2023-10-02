f2d48b21f3ec0a44fcbcbf951bd9847445358c5e
Hayden Plumley <mrhaydendp@gmail.com>
2023-10-02 09:43:41 -0700

    Update debloat.sh
    - Swapped echo with printf

--- Fire-Tools/debloat.sh
+++ Fire-Tools/debloat.sh
@@ -5,10 +5,10 @@ debloat () {
 case "$1" in
     Enable)
         adb shell pm enable "$2" 2> /dev/null ||
-        echo "Failed to Enable: $2";;
+        printf "%s\n" "Failed to Enable: $2";;
     Disable)
         adb shell pm disable-user "$2" 2> /dev/null && adb shell pm clear "$2" > /dev/null ||
-        echo "Failed to Disable: $2";;
+        printf "%s\n" "Failed to Disable: $2";;
 esac
 }
 
@@ -34,40 +34,40 @@ case "$opt" in
         done
         wait
         if [ "$opt" = "Enable" ]; then
-            echo "Disabling Adguard DNS"
+            printf "%s\n" "Disabling Adguard DNS"
             adb shell settings put global private_dns_mode -hostname
-            echo "Enabling Core Apps"
+            printf "%s\n" "Enabling Core Apps"
             export core="firelauncher device.software.ota device.software.ota.override kindle.otter.oobe.forced.ota"
             for package in ${core}; do
                 debloat Enable "com.amazon.$package"
             done
-            echo "Enabling Background Activities"
+            printf "%s\n" "Enabling Background Activities"
             adb shell settings put global always_finish_activities 0
-            echo "Successfully Enabled Fire OS Bloat"
+            printf "%s\n" "Successfully Enabled Fire OS Bloat"
         else
-            echo "Disabling Telemetry & Resetting Advertising ID"
+            printf "%s\n" "Disabling Telemetry & Resetting Advertising ID"
             adb shell settings put secure limit_ad_tracking 1
             adb shell settings put secure usage_metrics_marketing_enabled 0
             adb shell settings put secure USAGE_METRICS_UPLOAD_ENABLED 0
-            echo "Disabling Location"
+            printf "%s\n" "Disabling Location"
             adb shell settings put secure location_providers_allowed -network
-            echo "Blocking Ads With Adguard DNS"
+            printf "%s\n" "Blocking Ads With Adguard DNS"
             adb shell settings put global private_dns_mode hostname
             adb shell settings put global private_dns_specifier dns.adguard.com
-            echo "Disabling Lockscreen Ads"
+            printf "%s\n" "Disabling Lockscreen Ads"
             adb shell settings put global LOCKSCREEN_AD_ENABLED 0
-            echo "Disabling Search on Lockscreen"
+            printf "%s\n" "Disabling Search on Lockscreen"
             adb shell settings put secure search_on_lockscreen_settings 0
-            echo "Speeding Up Animations"
+            printf "%s\n" "Speeding Up Animations"
             adb shell settings put global window_animation_scale 0.50
             adb shell settings put global transition_animation_scale 0.50
             adb shell settings put global animator_duration_scale 0.50
             ram=$(adb shell grep "MemTotal" /proc/meminfo | awk '{print $2}')
             [ "$ram" -lt "1500000" ] && {
-                echo "Disabling Background Activities (< 1.5GB Ram)"
+                printf "%s\n" "Disabling Background Activities (< 1.5GB Ram)"
                 adb shell settings put global always_finish_activities 1
             }
-            echo "Successfully Debloated Fire OS"
+            printf "%s\n" "Successfully Debloated Fire OS"
         fi;;
     
     "Custom")
