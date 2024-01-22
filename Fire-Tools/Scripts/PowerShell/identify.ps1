d18e60758183dd72a354746443fffaf87d5bd9f9
Hayden Plumley <mrhaydendp@gmail.com>
2024-01-22 14:54:40 -0800

    Update identify.ps1
    - Swapped adb devices with echo command because its faster and provides an exit code to use
    - Added a check for modelNumber so $device doesn't end up being scrambled html

--- Fire-Tools/Scripts/PowerShell/identify.ps1
+++ Fire-Tools/Scripts/PowerShell/identify.ps1
@@ -1,11 +1,13 @@
 # Find Device Name by Regexing Model Number & Subtracting 3 Lines on Amazon Developer Docs
 $device = "Unknown/Undetected"
-if (adb devices){
+if (adb shell echo "Device Found" 2> $null){
     $model = (adb shell getprop ro.product.model)
     if (!(Test-Path .\ft-identifying-tablet-devices.html)){
         Invoke-RestMethod "https://developer.amazon.com/docs/fire-tablets/ft-identifying-tablet-devices.html" -OutFile ft-identifying-tablet-devices.html
     }
     $modelLine = (Select-String -Pattern "$model" -Path .\ft-identifying-tablet-devices.html).LineNumber
-    $device = (Get-Content .\ft-identifying-tablet-devices.html | Select -Index ("$modelLine" - 3) | Select-String "(Kindle|Fire) (.*?)[Gg]en\)").Matches.Value
+    if ($modelLine){
+        $device = (Get-Content .\ft-identifying-tablet-devices.html | Select -Index ("$modelLine" - 3) | Select-String "(Kindle|Fire) (.*?)[Gg]en\)").Matches.Value
+    }
 }
 Write-Host "Device: $device"
