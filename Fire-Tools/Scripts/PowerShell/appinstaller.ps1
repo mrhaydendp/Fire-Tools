f759ef16771eb477737558988b212d2d94326743
Hayden Plumley <mrhaydendp@gmail.com>
2024-01-24 14:50:18 -0800

    Update appinstaller.ps1
    - Simplified code by removing functions and using if for launcher config
    - Condensed Success/Fail message code to one line

--- Fire-Tools/Scripts/PowerShell/appinstaller.ps1
+++ Fire-Tools/Scripts/PowerShell/appinstaller.ps1
@@ -4,32 +4,22 @@ $option = $args[1]
 adb shell pm list packages -3 | Out-File packagelist
 
 # Change ADB Shell Arguments Based on Selection
-function appinstaller($app) {
-    Write-Host "Installing: $app"
-    if ("$app" -like '*.apk'){
-        adb install -g "$app" *> $null
-    } elseif ("$app" -like '*.apk*'){
-        Copy-Item "$app" -Destination "$app.zip"
-        Expand-Archive "$app.zip" -DestinationPath .\Split
-        adb install-multiple -r -g (Get-ChildItem .\Split\*apk) *> $null
-        Remove-Item -r .\Split, "$app.zip"
-    }
-    if ("$?" -eq "True"){
-        Write-Host "Success`n"
-    } else {
-        Write-Host "Fail`n"
-    }
+Write-Host "Installing: $app"
+if ("$app" -like '*.apk'){
+    adb install -g "$app" *> $null
+} elseif ("$app" -like '*.apk*'){
+    Copy-Item "$app" -Destination "$app.zip"
+    Expand-Archive "$app.zip" -DestinationPath .\Split
+    adb install-multiple -r -g (Get-ChildItem .\Split\*apk) *> $null
+    Remove-Item -r .\Split, "$app.zip"
 }
+if ("$?" -eq "True"){Write-Host "Success`n"} else {Write-Host "Fail`n"}
 
 # Grant Launcher Appwidget Permission & Attempt to Disable Fire Launcher. If Failed, Install LauncherHijack
-function launcherconfig {
+if ("$option" -eq "Launcher"){
     (Compare-Object (Get-Content .\packagelist) (adb shell pm list packages -3)).InputObject | Select -First 1 | % {
         adb shell appwidget grantbind --package "$_".split(":")[1]
     }
     adb shell pm disable-user -k com.amazon.firelauncher *> $null
-    if ("$?" -eq "False") {appinstaller .\LauncherHijackV403.apk}
+    if ("$?" -eq "False") {powershell -ExecutionPolicy Bypass -file .\Scripts\PowerShell\appinstaller .\LauncherHijackV403.apk}
 }
-
-# Call AppInstaller Function with .apk(m) to Install, If Launcher Argument is Passed Run Launcherconfig Function
-appinstaller "$app"
-if ($option -eq "Launcher"){launcherconfig}
