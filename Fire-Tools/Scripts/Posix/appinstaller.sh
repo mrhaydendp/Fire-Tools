dac969893d134ba8bb1f9134b942bea240a61f62
Hayden Plumley <mrhaydendp@gmail.com>
2024-01-24 14:53:06 -0800

    Update appinstaller.sh
    - Added "Launcher" argument to grant launcher Appwidget permission, disable Fire Launcher, and install LauncherHijack
    - Added basic support for LauncherHijack

--- Fire-Tools/Scripts/Posix/appinstaller.sh
+++ Fire-Tools/Scripts/Posix/appinstaller.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Export Package List to Compare After Installation
+adb shell pm list packages -3 > packagelist
+
 # Change Application Installation Method Based on Filetype
 printf "%s\n" "Installing: $1"
 case "$1" in
@@ -11,3 +14,12 @@ case "$1" in
     rm -rf ./Split;;
 esac
 [ "$?" = 0 ] && printf "%s\n\n" "Success" || printf "%s\n\n" "Fail"
+
+# Grant Launcher Appwidget Permission & Attempt to Disable Fire Launcher. If Failed, Install LauncherHijack
+if [ "$2" = "Launcher" ]; then
+    adb shell pm list packages -3 > packagelist.new
+    launcher=$(diff packagelist* | grep -E -o "[a-z0-9]*(\.[a-z0-9]+)+[a-z0-9]")
+    [ -n "$launcher" ] && adb shell appwidget grantbind --package "$launcher"
+    adb shell pm disable-user -k com.amazon.firelauncher >/dev/null 2>&1 ||
+    ./Scripts/Posix/appinstaller.sh LauncherHijackV403.apk
+fi
