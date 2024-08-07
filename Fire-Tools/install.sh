3eb553c385efe7068dc2cc8eeae59395a81bc4bd
Hayden Plumley <mrhaydendp@gmail.com>
2024-08-07 11:56:32 -0700

    VERY ROUGH but Working Install Script

--- Fire-Tools/install.sh
+++ Fire-Tools/install.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env sh
 
-# Determine OS
-# Check for Python3, adb, python3-tk
-# Input requirements.txt into Pip
 # Create .local/share/applications/fire-tools.desktop
 # Place Fire-Tools into .local/share/bin
 
-#Distros vars
-export supported="ubuntu, debian, fedora, arch"
-
-# Determine OS (Mac OS or Linux Distro)
-if [ ! -e /etc/os-release ]; then
-    echo "MacOS"
-else
-    for distro in $supported; do
-        grep "$distro" /etch/os
+# Determine package manager and run [word] command to install ADB & Python-TK
+linux_dependencies () {
+    export packagemanagers="apt dnf pacman"
+    for pm in ${packagemanagers}; do
+        which "$pm" && something="$pm"
     done
-fi
+    [ "$something" = "apt" ] && cmd="sudo apt install -y adb python3-tk"
+    [ "$something" = "dnf" ] && cmd="sudo dnf install -y android-tools python3-tkinter"
+    [ "$something" = "pacman" ] && cmd="sudo pacman -Sy android-tools tk"
+    exec $cmd
+}
+
+# Install Brew (https://brew.sh) and use it to install ADB & Python-TK
+mac_dependencies () {
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    yes | brew install android-platform-tools python-tk
+}
+
+# If Linux, run Linux function else run MacOS function
+[ "$(uname -s)" = "Linux" ] && linux_dependencies || mac_dependencies
+
+# Download "requirements.txt" and import into Pip
+curl -LO "https://raw.githubusercontent.com/mrhaydendp/Fire-Tools/main/Fire-Tools/requirements.txt"
+pip install -r requirements.txt
