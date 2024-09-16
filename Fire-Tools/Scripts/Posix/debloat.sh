ea10a3a74b1c7a115576f54578985c579940fa54
Hayden Plumley <mrhaydendp@gmail.com>
2024-09-16 09:27:06 -0700

    Update debloat.sh
    - Fixed accidental swap of $1 that was needed for a command
    - Fixed debloat function formatting
    - Fixed packages not being enabled/disabled due to incorrect variable being updated
    - Removed "Disable" being the default option

--- Fire-Tools/Scripts/Posix/debloat.sh
+++ Fire-Tools/Scripts/Posix/debloat.sh
@@ -3,19 +3,19 @@
 # Set Variables & Export Package List
 option="$1"
 app="$2"
-packages=$(awk '{print $option}' < Debloat.txt)
+packages=$(awk '{print $1}' < Debloat.txt)
 adb shell pm list packages -s > packagelist
 
 # Change ADB Shell Arguments Based on Selection
 debloat () {
-case "$option" in
-    Enable)
-        adb shell pm enable "$app" >/dev/null 2>&1;;
-    Disable)
-        adb shell pm disable-user "$app" >/dev/null 2>&1 && adb shell pm clear "$app" > /dev/null;;
-esac
-    # [Enable/Disable]d: package || Failed to [Enable/Disable]: Package
-    [ "$?" = 0 ] && printf "%sd: $app\n" "$option" || printf "%s\n" "Failed to $option: $app"
+    case "$option" in
+        Enable)
+            adb shell pm enable "$app" >/dev/null 2>&1;;
+        Disable)
+            adb shell pm disable-user "$app" >/dev/null 2>&1 && adb shell pm clear "$app" > /dev/null;;
+    esac
+        # Enable/Disable(d): package || Failed to Enable/Disable: package
+        [ "$?" = 0 ] && printf "%sd: $app\n" "$option" || printf "%s\n" "Failed to $option: $app"
 }
 
 # If a Package is Specified, Only run Debloat Function
@@ -23,8 +23,8 @@ if [ -n "$app" ]; then
     debloat "$option" "$app"
 else
     # Loop & Check if Package from Debloat.txt is Present in 'packagelist' if so, Send to the Debloat Function with Enable/Disable Option
-    for package in ${packages}; do
-        grep -q "$package" < packagelist && debloat "$option" "$package"
+    for app in ${packages}; do
+        grep -q "$package" < packagelist && debloat "$option" "$app"
     done
     if [ "$option" = "Enable" ]; then
         printf "%s\n" "Enabling Location"
@@ -37,7 +37,7 @@ else
             debloat Enable "com.amazon.$package"
         done
         printf "%s\n\n" "Successfully Enabled Fire OS Bloat"
-    else
+    elif [ "$option" = "Disable" ]; then
         printf "%s\n" "Disabling Telemetry & Resetting Advertising ID"
         adb shell settings put secure limit_ad_tracking 1
         adb shell settings put secure usage_metrics_marketing_enabled 0
