57b10a10e3cfc4ca05cff575266dd9b3adb88841
Hayden Plumley <mrhaydendp@gmail.com>
2025-07-19 19:40:28 -0700

    Update Device State Names

--- Fire-Tools/Scripts/Posix/identify.sh
+++ Fire-Tools/Scripts/Posix/identify.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
 # Set Placeholder Values for Device Name & Fire OS Version
-device="Unknown/Undetected"
-fireos="Unknown"
+device="Not Detected"
+fireos="N/A"
 
 # Find Device Name by Regexing Model Number & Subtracting 2 Lines on Amazon Developer Docs, Then get Fire OS Version
 if (adb shell echo "Device Found" >/dev/null 2>&1); then
+    if ! (adb shell getprop ro.build.mktg.fireos); then
+        device="Generic ADB"
     model=$(adb shell getprop ro.product.model)
     [ -e ft-identifying-tablet-devices.html ] || curl -O "https://developer.amazon.com/docs/fire-tablets/ft-identifying-tablet-devices.html"
     device=$(grep -B 2 "$model" < ft-identifying-tablet-devices.html | grep -E -o "(Kindle|Fire) (.*?)[G|g]en\)")
