052ddb0c7ef3657e2fbb8e2749d067cfe422cec5
Hayden Plumley <mrhaydendp@gmail.com>
2025-07-21 21:17:43 -0700

    Clean Up Build Commands
    - Added name, version, description, & copyright tags to binaries
    - Split command into multi-line to increase readability

---
+++ build.ps1
@@ -0,0 +1,8 @@
+nuitka ./Fire-Tools/main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter `
+--product-name="Fire Tools" --product-version=25.07 --copyright="Hayden Plumley & Contributors" --file-description="Fire Tools" `
+--include-data-dir=./Fire-Tools/Batch=./Batch `
+--include-data-dir=./Fire-Tools/Extracted=./Extracted `
+--include-data-dir=./Fire-Tools/Gapps=./Gapps `
+--include-data-dir=./Fire-Tools/Scripts=./Scripts `
+--include-data-files=./Fire-Tools/Debloat.txt=./ `
+--include-data-files=./Fire-Tools/*.apk=./
--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
 # Build into Seperate Folder with Batch, Gapps, Extracted, Scripts, and Debloat.txt Included
-nuitka ./Fire-Tools/main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter --include-data-dir=./Fire-Tools/Batch=./Batch --include-data-dir=./Fire-Tools/Extracted=./Extracted --include-data-dir=./Fire-Tools/Gapps=./Gapps --include-data-dir=./Fire-Tools/Scripts=./Scripts --include-data-files=./Fire-Tools/Debloat.txt=./ --include-data-files=./Fire-Tools/*.apk=./
+nuitka ./Fire-Tools/main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter \
+--product-name="Fire Tools" --product-version=25.07 --copyright="Hayden Plumley & Contributors" --file-description="Fire Tools" \
+--include-data-dir=./Fire-Tools/Batch=./Batch \
+--include-data-dir=./Fire-Tools/Extracted=./Extracted \
+--include-data-dir=./Fire-Tools/Gapps=./Gapps \
+--include-data-dir=./Fire-Tools/Scripts=./Scripts \
+--include-data-files=./Fire-Tools/Debloat.txt=./ \
+--include-data-files=./Fire-Tools/*.apk=./
