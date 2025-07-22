f148e3e9100a6f32ac46d6fabde3ed1850d67498
Hayden Plumley <mrhaydendp@gmail.com>
2025-07-21 22:30:56 -0700

    Update Build Scripts

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 
+# Set Application Tags
+name="Fire Tools"
+version="25.07"
+author="Hayden Plumley & Contributors"
+
 # Build into Seperate Folder with Batch, Gapps, Extracted, Scripts, and Debloat.txt Included
 nuitka ./Fire-Tools/main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter \
---product-name="Fire Tools" --product-version=25.07 --copyright="Hayden Plumley & Contributors" --file-description="Fire Tools" \
+--product-name="$name" --product-version=25.07 --copyright="$author" --file-description="$name" \
 --include-data-dir=./Fire-Tools/Batch=./Batch \
 --include-data-dir=./Fire-Tools/Extracted=./Extracted \
 --include-data-dir=./Fire-Tools/Gapps=./Gapps \
