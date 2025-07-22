d3c69f28a3fa2f38bffcc0229f5ef0555623326b
Hayden Plumley <mrhaydendp@gmail.com>
2025-07-21 23:10:40 -0700

    Update Build Scripts
    - Added path variable

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@
 name="Fire Tools"
 version="25.07"
 author="Hayden Plumley & Contributors"
+path="./Fire-Tools"
 
 # Build into Seperate Folder with Batch, Gapps, Extracted, Scripts, and Debloat.txt Included
 nuitka ./Fire-Tools/main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter \
 --product-name="$name" --product-version=25.07 --copyright="$author" --file-description="$name" \
---include-data-dir=./Fire-Tools/Batch=./Batch \
---include-data-dir=./Fire-Tools/Extracted=./Extracted \
---include-data-dir=./Fire-Tools/Gapps=./Gapps \
---include-data-dir=./Fire-Tools/Scripts=./Scripts \
---include-data-files=./Fire-Tools/Debloat.txt=./ \
---include-data-files=./Fire-Tools/*.apk=./
+--include-data-dir="$path/Batch"=./Batch \
+--include-data-dir="$path/Extracted"=./Extracted \
+--include-data-dir="$path/Gapps"=./Gapps \
+--include-data-dir="$path/Scripts"=./Scripts \
+--include-data-files="$path/Debloat.txt"=./ \
+--include-data-files="$path/*.apk"=./
