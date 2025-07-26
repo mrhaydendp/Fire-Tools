782123e65df0eea9638597cc308b35b669c1c386
Hayden Plumley <mrhaydendp@gmail.com>
2025-07-25 21:01:49 -0700

    Update build.ps1
    - Version bump
    - Nuitka now uses python module mode
    - Windows version now only includes PowerShell scripts

--- build.ps1
+++ build.ps1
@@ -1,15 +1,15 @@
 # Set Application Tags
 $name = "Fire Tools"
-$version = "25.07.1"
+$version = "25.08"
 $author = "Hayden Plumley & Contributors"
 $path=".\Fire-Tools"
 
 # Build into Seperate Folder with Batch, Gapps, Extracted, Scripts, and Debloat.txt Included
-nuitka $path\main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter `
+python -m nuitka $path\main.py --output-filename=fire-tools --mode=standalone --enable-plugin=tk-inter `
 --product-name="$name" --product-version="$version" --copyright="$author" --file-description="$name" `
 --include-data-dir="$path\Batch"=.\Batch `
 --include-data-dir="$path\Extracted"=.\Extracted `
 --include-data-dir="$path\Gapps"=.\Gapps `
---include-data-dir="$path\Scripts"=.\Scripts `
+--include-data-dir="$path\Scripts\PowerShell"=.\Scripts\PowerShell `
 --include-data-files="$path\Debloat.txt"=.\ `
 --include-data-files="$path\*.apk"=.\
