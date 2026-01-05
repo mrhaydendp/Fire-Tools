af21d25657594256405905b0f687cd412f412fa7
Hayden Plumley <mrhaydendp@gmail.com>
2026-01-05 09:33:32 -0800

    Version Bump to 26.01

--- Fire-Tools/main.py
+++ Fire-Tools/main.py
@@ -9,7 +9,7 @@ import customtkinter as ctk
 chdir(path.dirname(path.realpath(__file__)))
 
 # Platform Variables
-version = "25.11"
+version = "26.01"
 platform = "Linux/macOS"
 default_path = f"{getcwd()}/Scripts/Posix/"
 extension = ".sh"
